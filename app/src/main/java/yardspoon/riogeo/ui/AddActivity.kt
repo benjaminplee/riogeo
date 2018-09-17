9627dddd5a3e1303ac4ef55cad23a4f0771b6499
BenjaminPLee <benjaminplee@gmail.com>
2018-09-17 16:30:23 -0500

    Adding add activity

---
+++ app/src/main/java/yardspoon/riogeo/ui/AddActivity.kt
@@ -0,0 +1,13 @@
+package yardspoon.riogeo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import yardspoon.riogeo.R
+
+class AddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add)
+    }
+}
