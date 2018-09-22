0488c239c73a0cc50ea4aa0889dbb77296676cbd
BenjaminPLee <benjaminplee@gmail.com>
2018-09-22 15:30:12 -0500

    starting onboarding

---
+++ app/src/main/java/yardspoon/riogeo/ui/OnboardingActivity.kt
@@ -0,0 +1,14 @@
+package yardspoon.riogeo.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import yardspoon.riogeo.R
+
+class OnboardingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_onboarding)
+    }
+
+}
