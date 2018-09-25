14f91a2be38d0895083c89ff498b8e26470b4a5a
BenjaminPLee <benjaminplee@gmail.com>
2018-09-25 16:15:33 -0500

    0000 - Add timber - BL

--- app/src/main/java/yardspoon/riogeo/RioGeoApp.kt
+++ app/src/main/java/yardspoon/riogeo/RioGeoApp.kt
@@ -5,6 +5,7 @@ import org.koin.android.ext.android.startKoin
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.ext.koin.viewModel
 import org.koin.dsl.module.module
+import timber.log.Timber
 import yardspoon.riogeo.biz.GeofencesViewModel
 import yardspoon.riogeo.data.AppDatabase
 
@@ -19,6 +20,10 @@ class RioGeoApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
 
         startKoin(this, listOf(appModule))
     }
