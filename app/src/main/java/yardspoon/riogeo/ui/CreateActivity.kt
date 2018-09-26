95115285b773f591afa299e24c7dcf059328a52f
BenjaminPLee <benjaminplee@gmail.com>
2018-09-25 20:51:03 -0500

    Working on map and search

--- app/src/main/java/yardspoon/riogeo/ui/CreateActivity.kt
+++ app/src/main/java/yardspoon/riogeo/ui/CreateActivity.kt
@@ -4,16 +4,21 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.places.Place
+import com.google.android.gms.location.places.ui.PlaceSelectionListener
+import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import timber.log.Timber
 import yardspoon.riogeo.R
 import yardspoon.riogeo.misc.permissionGiven
 
-class CreateActivity : AppCompatActivity(), OnMapReadyCallback {
+class CreateActivity : AppCompatActivity(), OnMapReadyCallback, PlaceSelectionListener {
 
     private lateinit var map: GoogleMap
 
@@ -21,7 +26,11 @@ class CreateActivity : AppCompatActivity(), OnMapReadyCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        val mapFragment = findMapFragment()
+        val searchFragment = findAutocompleteSearchFragment()
+
+        searchFragment.setOnPlaceSelectedListener(this)
+
         mapFragment.getMapAsync(this)
     }
 
@@ -32,12 +41,25 @@ class CreateActivity : AppCompatActivity(), OnMapReadyCallback {
                 isMyLocationEnabled = true
             }
 
-
-
             val sydney = LatLng(-34.0, 151.0)
             addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
             moveCamera(CameraUpdateFactory.newLatLng(sydney))
         }
     }
+
+    override fun onPlaceSelected(place: Place?) {
+        place?.let {
+            Timber.i("Autocomplete search resulted in place: $place")
+            map.addMarker(MarkerOptions().position(place.latLng).title(place.name.toString()))
+            map.animateCamera(CameraUpdateFactory.newLatLng(place.latLng))
+        }
+    }
+
+    override fun onError(status: Status?) {
+        Timber.e("Problem searching for place with autocomplete: $status")
+    }
+
+    private fun findMapFragment() = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+    private fun findAutocompleteSearchFragment() = supportFragmentManager.findFragmentById(R.id.autocomplete_search) as SupportPlaceAutocompleteFragment
 
 }
