d090f727cc37d329182bcb303bc9bb4f93bbc499
BenjaminPLee <benjaminplee@gmail.com>
2018-09-27 14:32:23 -0500

    Moving marker

--- app/src/main/java/yardspoon/riogeo/ui/CreateActivity.kt
+++ app/src/main/java/yardspoon/riogeo/ui/CreateActivity.kt
@@ -15,17 +15,20 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import timber.log.Timber
 import yardspoon.riogeo.R
 import yardspoon.riogeo.misc.permissionGiven
 
 private val WHITE_HOUSE_LOC = LatLng(38.8976805, -77.0387185)
+private const val HIGH_ZOOM_LEVEL = 16.0f
 
 class CreateActivity : AppCompatActivity(), OnMapReadyCallback, PlaceSelectionListener {
 
     private lateinit var map: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private var marker: Marker? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,20 +45,31 @@ class CreateActivity : AppCompatActivity(), OnMapReadyCallback, PlaceSelectionLi
         map = googleMap.apply {
             if (permissionGiven(Manifest.permission.ACCESS_FINE_LOCATION)) {
                 isMyLocationEnabled = true
-            }
 
-            zoomMapTo(WHITE_HOUSE_LOC, "White House")
+                fusedLocationClient.lastLocation
+                        .addOnSuccessListener { zoomMapTo(LatLng(it.latitude, it.longitude), null) }
+                        .addOnFailureListener { zoomMapTo(WHITE_HOUSE_LOC, "The White House") }
+            }
         }
     }
 
     override fun onPlaceSelected(place: Place?) {
-        place?.let {
+        place?.apply {
             Timber.i("Autocomplete search resulted in place: $place")
-            map.addMarker(MarkerOptions().position(place.latLng).title(place.name.toString()))
-            map.animateCamera(CameraUpdateFactory.newLatLng(place.latLng))
+            map.zoomMapTo(latLng, name.toString())
         }
     }
 
+    private fun GoogleMap.zoomMapTo(placeLocation: LatLng, placeName: String? = null) {
+        marker = (marker ?: addMarker(MarkerOptions().position(placeLocation).draggable(true))).apply {
+            position = placeLocation
+            title = placeName
+        }
+
+        animateCamera(CameraUpdateFactory.newLatLngZoom(placeLocation, HIGH_ZOOM_LEVEL))
+    }
+
+    // Place autocomplete
     override fun onError(status: Status?) {
         Timber.e("Problem searching for place with autocomplete: $status")
     }
@@ -64,9 +78,3 @@ class CreateActivity : AppCompatActivity(), OnMapReadyCallback, PlaceSelectionLi
     private fun findAutocompleteSearchFragment() = supportFragmentManager.findFragmentById(R.id.autocomplete_search) as SupportPlaceAutocompleteFragment
 
 }
-
-private fun GoogleMap.zoomMapTo(location: LatLng, title: String?) {
-    addMarker(MarkerOptions().position(location).title(title))
-    moveCamera(CameraUpdateFactory.newLatLng(location))
-}
-
